57f8bbaf7e1649e9f7ab2b0f74bcb36bb88573aa
Daphne210 <dapnephinerry2@gmail.com>
2025-06-23 22:26:39 +0300

    Add HMIS ACP 039 HIVST Register

--- api/src/main/java/org/openmrs/module/ugandaemr/api/deploy/bundle/CommonMetadataBundle.java
+++ api/src/main/java/org/openmrs/module/ugandaemr/api/deploy/bundle/CommonMetadataBundle.java
@@ -50,7 +50,6 @@ public class CommonMetadataBundle extends AbstractMetadataBundle {
         install(PersonAttributeTypes.OCCUPATION);
         install(PersonAttributeTypes.NATIONALITY);
         install(PersonAttributeTypes.EMAIL_ADDRESS);
-        install(PersonAttributeTypes.EDUCATION_LEVEL);
         log.info("Person AttributeTypes installed");
 
         // install roles
@@ -104,7 +103,7 @@ public class CommonMetadataBundle extends AbstractMetadataBundle {
         install(EncounterTypes.MEDICATION_ORDER);
         install(EncounterTypes.TB_SCREENING_ENCOUNTER);
         install(EncounterTypes.MONKEY_POX_SCREENING_ENCOUNTER);
-        install(EncounterTypes.PROCEDURE_RESULTS_ENCOUNTER);
+        install(EncounterTypes.HIV_SELF_TESTING_ENCOUNTER);
 
 
         //installing programs metadata
